7dd0b2084ec4de5eadada2bd2679df13cfa41975
Alexandre M Assis <amassis@outlook.com>
2018-08-08 00:33:38 -0300

    corrected package.json

---
+++ db/adminUser.sql
@@ -0,0 +1,3 @@
+INSERT INTO chaosdb.sys_users
+(username, email, fullname, password, active)
+VALUES('admin@hackingbees.tech', 'admin@hackingbees.tech', 'HackingBees Admin', '$2b$10$o1seiMGBu8ItaaIGsWx0vOtCe7Fplsy94A5lpP6QZQofAataU41Ny', 0);
